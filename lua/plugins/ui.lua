c313b6b6d5c0e8c642bc335ebaaac0a07b3eebfa
taloric <taloricag@gmail.com>
2024-12-20 09:59:41 +0800

    allow neotree show filtered items

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -191,6 +191,9 @@ return {
 				bind_to_cwd = false,
 				follow_current_file = { enabled = true },
 				use_libuv_file_watcher = true,
+				filtered_items = {
+					visible = true,
+				},
 			},
 			window = {
 				-- defaut width: 40
